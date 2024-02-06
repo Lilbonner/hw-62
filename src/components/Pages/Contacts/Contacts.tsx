836599f12e3a252f7a106683640913f8e8cd34d1
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-06 18:45:31 +0600

    edit contact component

--- src/components/Pages/Contacts/Contacts.tsx
+++ src/components/Pages/Contacts/Contacts.tsx
@@ -1,8 +1,10 @@
-
 const Contacts = () => {
     return (
         <div>
-
+            <p className="text-3xl ml-20 mt-14 font-bold">Me in:</p>
+            <a href="https://github.com/Lilbonner?tab=repositories" className="text-2xl ml-24 mt-14">GitHub: Lilbonner</a>
+            <br/>
+            <a href="https://www.instagram.com/5kein/" className="text-2xl ml-24 mt-6">inst: 5kein</a>
         </div>
     );
 };
