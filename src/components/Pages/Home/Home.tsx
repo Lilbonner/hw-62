beed3e43cd5615e372109517f954a8a40fdcf61b
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-06 18:45:06 +0600

    edit home component

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,8 +1,13 @@
-
 const Home = () => {
     return (
         <div>
+            <h1 className="font-bold text-4xl ml-24 mt-20">
+                Hello, my name is Nasirov Daniyar.
+                <h2 className="mt-3">i'm aspiring developer & cyclist.</h2>
+            </h1>
+            <p>
 
+            </p>
         </div>
     );
 };
