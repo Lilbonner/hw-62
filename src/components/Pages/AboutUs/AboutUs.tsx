02bf3fa3c7de4ba67dc60617faee0dc350352249
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-06 18:46:04 +0600

    edit about component

--- src/components/Pages/AboutUs/AboutUs.tsx
+++ src/components/Pages/AboutUs/AboutUs.tsx
@@ -1,8 +1,14 @@
-
 const AboutUs = () => {
     return (
         <div>
-
+            <h3 className="text-4xl font-bold mt-14 ml-10">About me</h3>
+            <p className="text-3xl ml-16 mt-11">
+                I'm attending Attractor School to study frontend in web programming. <br/>
+                I trying to expand my programming skills and find my way in the field. <br/>
+                I am enthusiastic about learning different technologies and tools related to web development,<br/>
+                and I dream of building creative and innovative projects.<br/><br/>
+                I am a fan of fixed-gear bicycles and I often sell them for a profit.
+            </p>
         </div>
     );
 };
